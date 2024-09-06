6ead5d15428cff464733b534e22c23db63996edb
Ankit7rma <ankitsharma290101@gmail.com>
2024-09-06 20:01:41 +0530

    faq

--- components/FAQ/faqData.tsx
+++ components/FAQ/faqData.tsx
@@ -9,27 +9,27 @@ const faqData: FAQ[] = [
   {
     id: 2,
     quest: "What is the use of coupon codes?",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    ans: "While doing online shopping a coupon code is redeemed at the checkout to get an extra discount on a shopper's purchase.",
   },
   {
     id: 3,
     quest: "Are promo codes worth it?",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    ans: "Yes! The promo codes are worth it! It gives extra discounts to customers on their online purchases.",
   },
   {
     id: 4,
     quest: "Is it safe to use coupon codes online?",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    ans: "It is completely safe to use coupon codes online. Because online retailers give them to coupon websites like CouponBaba.com",
   },
   {
     id: 5,
-    quest: "Are promo codes worth it?",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "How do I get a coupon code?",
+    ans: "You will get a coupon code from an authoritative coupon website like CouponMoto. Visit couponmoto.com website, and search for any online stores of your choice, you will get coupon codes & deals for them.",
   },
   {
     id: 6,
-    quest: "Are promo codes worth it?",
-    ans: "We provide digital experience services to startups and small businesses. We help our clients succeed by creating brand identities, digital experiences.",
+    quest: "How do you use a coupon code for shopping?",
+    ans: "Just copy the coupon code from the coupon website, and paste it to the cart page during the checkout process. The code will be redeemed & you will get a discount on the purchase.",
   },
 ];
 
